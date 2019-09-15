d7d287a5985a80bea8e14a86e2f2659f9da52cb3
perfectsquircle <calvinfurano@gmail.com>
2019-09-15 04:33:21 -0600

    Test nullish mapping.

--- src/test/java/toadstool/mapper/ClassResultSetMapperTests.java
+++ src/test/java/toadstool/mapper/ClassResultSetMapperTests.java
@@ -31,12 +31,14 @@ public class ClassResultSetMapperTests {
         var stockPrice = 77.77d;
         var createDate = new java.sql.Date(999);
         var resultSetMetadata = mock(ResultSetMetaData.class);
-        when(resultSetMetadata.getColumnCount()).thenReturn(5);
+        when(resultSetMetadata.getColumnCount()).thenReturn(7);
         when(resultSetMetadata.getColumnName(1)).thenReturn("id");
         when(resultSetMetadata.getColumnName(2)).thenReturn("stock_price");
         when(resultSetMetadata.getColumnName(3)).thenReturn("name");
         when(resultSetMetadata.getColumnName(4)).thenReturn("create_date");
         when(resultSetMetadata.getColumnName(5)).thenReturn("cant_touch_this");
+        when(resultSetMetadata.getColumnName(6)).thenReturn("integer_bob");
+        when(resultSetMetadata.getColumnName(7)).thenReturn("nullable_bob");
         var resultSet = mock(ResultSet.class);
         when(resultSet.getMetaData()).thenReturn(resultSetMetadata);
         when(resultSet.getInt("id")).thenReturn(id);
@@ -44,6 +46,8 @@ public class ClassResultSetMapperTests {
         when(resultSet.getDouble("stock_price")).thenReturn(stockPrice);
         when(resultSet.getDate("create_date")).thenReturn(createDate);
         when(resultSet.getString("cant_touch_this")).thenReturn("nope");
+        when(resultSet.getObject("integer_bob")).thenReturn((Integer) 3);
+        when(resultSet.getObject("nullable_bob")).thenReturn((Integer) null);
         var mapper = new ClassResultSetMapper();
 
         // When
@@ -56,6 +60,8 @@ public class ClassResultSetMapperTests {
         assertEquals(stockPrice, result.getStockPrice(), 0.1d);
         assertEquals(createDate, result.getCreateDate());
         assertEquals("Stop. Hammer time.", result.getCantTouchThis());
+        assertEquals((Integer) 3, result.getIntegerBob());
+        assertEquals((Integer) null, result.getNullableBob());
 
     }
 }
